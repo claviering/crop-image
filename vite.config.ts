a55a789338511a1b271fc58c9e59a4b392ca9858
claviering <claviering@gmail.com>
2023-03-11 17:42:32 +0800

    build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import typescript from "@rollup/plugin-typescript";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [typescript()],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
